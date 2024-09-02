3ec8eb91a142609db86647803708792ee31f6ba2
Shrey Nagda <shreynagda2714@gmail.com>
2024-09-02 11:04:38 +0530

    Footer design fixed

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-black">
+    <footer className="bg-black text-center p-4 text-white ">
       <p>&copy; 2024 ClubConnect </p>
     </footer>
   );
