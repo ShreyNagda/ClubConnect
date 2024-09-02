02060f456682dbc722cf33a2b60917a0e44208b8
Shrey Nagda <shreynagda2714@gmail.com>
2024-09-02 10:43:23 +0530

    Cards page edited

--- frontend/src/Clubs.jsx
+++ frontend/src/Clubs.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./Clubs.css";
 
 function Clubs() {
   return (
-    <div className="clubs-container">
-      <header>
-        <h1>Clubs</h1>
+    <div className="p-2">
+      <header className="p-3 bg-[#337ab7]">
+        <h2 className="text-3xl font-bold">Clubs</h2>
       </header>
-      <section className="about-clubs">
-        <h2>About Our Clubs</h2>
+      <section className="p-3">
+        <h2 className="text-2xl font-semibold py-1">About Our Clubs</h2>
+        <img src="club1-logo.png" alt="" />
         <p>
           At our college, students have the opportunity to join a wide range of
           clubs that cater to diverse interests and passions. Whether you're
@@ -18,28 +18,7 @@ function Clubs() {
           enhance your campus life!
         </p>
       </section>
-      <section className="club-tiles">
-        <div className="row">
-          <div className="club-tile">
-            <img src="club1-logo.jpg" alt="Club 1 Logo" />
-            <h3>
-              <a href="/club1">Club 1</a>
-            </h3>
-          </div>
-          <div className="club-tile">
-            <img src="logo2.png" alt="Club 2 Logo" />
-            <h3>
-              <a href="/club2">Club 2</a>
-            </h3>
-          </div>
-          <div className="club-tile">
-            <img src="logo3.png" alt="Club 3 Logo" />
-            <h3>
-              <a href="/club3">Club 3</a>
-            </h3>
-          </div>
-        </div>
-      </section>
+      <section className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 items-center"></section>
     </div>
   );
 }
