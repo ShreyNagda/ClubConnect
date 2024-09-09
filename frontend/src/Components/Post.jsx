cb4d9e2a7678f6de29845d725670669aae2b3e4c
Shrey Nagda <shreynagda2714@gmail.com>
2024-09-09 10:36:21 +0530

    Posts edited

--- frontend/src/Components/Post.jsx
+++ frontend/src/Components/Post.jsx
@@ -2,7 +2,7 @@ function Post(props) {
   return (
     <div className="p-5 bg-[#fff] rounded-md">
       <img src={props.src} alt="" className="aspect-square object-cover" />
-      <p className="text-black text-2xl">{props.text}</p>
+      <p className="text-black text-2xl mt-2">{props.text}</p>
     </div>
   );
 }
